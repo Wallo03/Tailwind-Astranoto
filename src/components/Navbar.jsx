9da2050fd631e178634258439d1dd059131a406a
Wallo03 <walid.birdaha03@gmail.com>
2023-05-24 00:20:25 +0200

    upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className='w-full text-3xl font-bold text-blue'>BIRD</h1>
+      <h1 className='w-full text-3xl font-bold text-[#80BDE3]'>BIRD</h1>
       <ul className='flex'>
           <li className='p-5'>Home</li>
           <li className='p-5'>Company</li>
